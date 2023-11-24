08c797072e1fbe0c714264ace62eba8661288bb1
Aleksandr <peenky88@yandex.ru>
2023-11-24 16:39:23 +0500

    изм импортов

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 import './page.css'
-import Promo from '@/components/promo/Promo'
-import Brands from '@/components/brands/brands'
-import Arrivials from '@/components/arrivials/Arrivials'
-import Payday from '@/components/payday/Payday'
-import Favourite from '@/components/favourite/Favourite'
-import Phone from '@/components/phone/Phone'
-import Accession from '@/components/accession/Accession'
-import Footer from '@/components/footer/Footer'
+import Promo from '../components/promo/Promo'
+import Brands from '../components/brands/brands'
+import Arrivials from '../components/arrivials/Arrivials'
+import Payday from '../components/payday/Payday'
+import Favourite from '../components/favourite/Favourite'
+import Phone from '../components/phone/Phone'
+import Accession from '../components/accession/Accession'
+import Footer from '../components/footer/Footer'
 
 export default function Home() {
   return (
